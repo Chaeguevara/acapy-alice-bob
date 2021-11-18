40e1c28728c8c3438686ed5ded0968b6cbc79303
Chaeguevara <dfso2@naver.com>
2021-11-18 20:36:04 +0900

    first commit

--- demo/runners/alice.py
+++ demo/runners/alice.py
@@ -1,11 +1,11 @@
 import asyncio
-import base64
-import binascii
 import json
 import logging
 import os
+import random
 import sys
-from urllib.parse import urlparse
+
+from uuid import uuid4
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  # noqa
 
@@ -20,28 +20,29 @@ from runners.support.utils import (
     require_indy,
 )
 
+CRED_PREVIEW_TYPE = (
+    "did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/issue-credential/1.0/credential-preview"
+)
 
 LOGGER = logging.getLogger(__name__)
 
 
-class AliceAgent(DemoAgent):
+class FaberAgent(DemoAgent):
     def __init__(self, http_port: int, admin_port: int, **kwargs):
         super().__init__(
-            "Alice Agent",
+            "Faber Agent",
             http_port,
             admin_port,
-            prefix="Alice",
-            extra_args=[
-                "--auto-accept-invites",
-                "--auto-accept-requests",
-                "--auto-store-credential",
-            ],
-            seed=None,
+            prefix="Faber",
+            extra_args=["--auto-accept-invites", "--auto-accept-requests"],
             **kwargs,
         )
         self.connection_id = None
         self._connection_ready = asyncio.Future()
         self.cred_state = {}
+        # TODO define a dict to hold credential attributes
+        # based on credential_definition_id
+        self.cred_attrs = {}
 
     async def detect_connection(self):
         await self._connection_ready
@@ -71,135 +72,46 @@ class AliceAgent(DemoAgent):
             credential_exchange_id,
         )
 
-        if state == "offer_received":
-            log_status("#15 After receiving credential offer, send credential request")
+        if state == "request_received":
+            log_status("#17 Issue credential to X")
+            # issue credentials based on the credential_definition_id
+            cred_attrs = self.cred_attrs[message["credential_definition_id"]]
+            cred_preview = {
+                "@type": CRED_PREVIEW_TYPE,
+                "attributes": [
+                    {"name": n, "value": v} for (n, v) in cred_attrs.items()
+                ],
+            }
             await self.admin_POST(
-                "/issue-credential/records/" f"{credential_exchange_id}/send-request"
+                f"/issue-credential/records/{credential_exchange_id}/issue",
+                {
+                    "comment": f"Issuing credential, exchange {credential_exchange_id}",
+                    "credential_preview": cred_preview,
+                },
             )
-
-        elif state == "credential_acked":
-            self.log("Stored credential {cred_id} in wallet")
-            cred_id = message["credential_id"]
-            log_status(f"#18.1 Stored credential {cred_id} in wallet")
-            resp = await self.admin_GET(f"/credential/{cred_id}")
-            log_json(resp, label="Credential details:")
-            log_json(
-                message["credential_request_metadata"],
-                label="Credential request metadata:",
-            )
-            self.log("credential_id", message["credential_id"])
-            self.log("credential_definition_id", message["credential_definition_id"])
-            self.log("schema_id", message["schema_id"])
 
     async def handle_present_proof(self, message):
         state = message["state"]
-        presentation_exchange_id = message["presentation_exchange_id"]
-        presentation_request = message["presentation_request"]
 
-        log_msg(
+        presentation_exchange_id = message["presentation_exchange_id"]
+        self.log(
             "Presentation: state =",
             state,
             ", presentation_exchange_id =",
             presentation_exchange_id,
         )
 
-        if state == "request_received":
-            log_status(
-                "#24 Query for credentials in the wallet that satisfy the proof request"
+        if state == "presentation_received":
+            log_status("#27 Process the proof provided by X")
+            log_status("#28 Check if proof is valid")
+            proof = await self.admin_POST(
+                f"/present-proof/records/{presentation_exchange_id}/"
+                "verify-presentation"
             )
-
-            # include self-attested attributes (not included in credentials)
-            credentials_by_reft = {}
-            revealed = {}
-            self_attested = {}
-            predicates = {}
-
-            # select credentials to provide for the proof
-            credentials = await self.admin_GET(
-                f"/present-proof/records/{presentation_exchange_id}/credentials"
-            )
-            if credentials:
-                for row in credentials:
-                    for referent in row["presentation_referents"]:
-                        if referent not in credentials_by_reft:
-                            credentials_by_reft[referent] = row
-
-            for referent in presentation_request["requested_attributes"]:
-                if referent in credentials_by_reft:
-                    revealed[referent] = {
-                        "cred_id": credentials_by_reft[referent]["cred_info"][
-                            "referent"
-                        ],
-                        "revealed": True,
-                    }
-                else:
-                    self_attested[referent] = "my self-attested value"
-
-            for referent in presentation_request["requested_predicates"]:
-                if referent in credentials_by_reft:
-                    predicates[referent] = {
-                        "cred_id": credentials_by_reft[referent]["cred_info"][
-                            "referent"
-                        ],
-                        "revealed": True,
-                    }
-
-            log_status("#25 Generate the proof")
-            request = {
-                "requested_predicates": predicates,
-                "requested_attributes": revealed,
-                "self_attested_attributes": self_attested,
-            }
-
-            log_status("#26 Send the proof to X")
-            await self.admin_POST(
-                (
-                    "/present-proof/records/"
-                    f"{presentation_exchange_id}/send-presentation"
-                ),
-                request,
-            )
+            self.log("Proof =", proof["verified"])
 
     async def handle_basicmessages(self, message):
         self.log("Received message:", message["content"])
-
-
-async def input_invitation(agent):
-    async for details in prompt_loop("Invite details: "):
-        b64_invite = None
-        try:
-            url = urlparse(details)
-            query = url.query
-            if query and "c_i=" in query:
-                pos = query.index("c_i=") + 4
-                b64_invite = query[pos:]
-            else:
-                b64_invite = details
-        except ValueError:
-            b64_invite = details
-
-        if b64_invite:
-            try:
-                invite_json = base64.urlsafe_b64decode(b64_invite)
-                details = invite_json.decode("utf-8")
-            except binascii.Error:
-                pass
-            except UnicodeDecodeError:
-                pass
-
-        if details:
-            try:
-                json.loads(details)
-                break
-            except json.JSONDecodeError as e:
-                log_msg("Invalid invitation:", str(e))
-
-    with log_timer("Connect duration:"):
-        connection = await agent.admin_POST("/connections/receive-invitation", details)
-        agent.connection_id = connection["connection_id"]
-        log_json(connection, label="Invitation response:")
-
-        await agent.detect_connection()
 
 
 async def main(start_port: int, show_timing: bool = False):
@@ -212,36 +124,130 @@ async def main(start_port: int, show_timing: bool = False):
     agent = None
 
     try:
-        log_status("#7 Provision an agent and wallet, get back configuration details")
-        agent = AliceAgent(
+        log_status("#1 Provision an agent and wallet, get back configuration details")
+        agent = FaberAgent(
             start_port, start_port + 1, genesis_data=genesis, timing=show_timing
         )
         await agent.listen_webhooks(start_port + 2)
+        await agent.register_did()
 
         with log_timer("Startup duration:"):
             await agent.start_process()
         log_msg("Admin url is at:", agent.admin_url)
         log_msg("Endpoint url is at:", agent.endpoint)
 
-        log_status("#9 Input faber.py invitation details")
-        await input_invitation(agent)
+        # Create a schema
+        with log_timer("Publish schema/cred def duration:"):
+            log_status("#3/4 Create a new schema/cred def on the ledger")
+            version = format(
+                "%d.%d.%d"
+                % (
+                    random.randint(1, 101),
+                    random.randint(1, 101),
+                    random.randint(1, 101),
+                )
+            )
+            (
+                _,  # schema id
+                credential_definition_id,
+            ) = await agent.register_schema_and_creddef(
+                "degree schema", version, ["name", "date", "usertype", "average","visitcount","rewardcount"]
+            )
+
+        # TODO add an additional credential for Student ID
+
+        # with log_timer("Generate invitation duration:"):
+        #     # Generate an invitation
+        #     log_status(
+        #         "#5 Create a connection to alice and print out the invite details"
+        #     )
+        #     connection = await agent.admin_POST("/connections/create-invitation")
+
+        # agent.connection_id = connection["connection_id"]
+        # log_json(connection, label="Invitation response:")
+        # log_msg("*****************")
+        # log_msg(json.dumps(connection["invitation"]), label="Invitation:", color=None)
+        # log_msg("*****************")
+
+        # log_msg("Waiting for connection...")
+        # await agent.detect_connection()
 
         async for option in prompt_loop(
-            "(3) Send Message (4) Input New Invitation (X) Exit? [3/4/X]: "
+            "(1) Issue Credential, (2) Send Proof Request, "
+            + "(3) Send Message (X) Exit? [1/2/3/X] "
         ):
             if option is None or option in "xX":
                 break
+
+            elif option == "1":
+                log_status("#13 Issue credential offer to X")
+
+                # TODO define attributes to send for credential
+                agent.cred_attrs[credential_definition_id] = {
+                    "name": "Alice Smith",
+                    "date": "2018-05-28",
+                    "UserType": "Visitor",
+                    "age": "24",
+                }
+
+                cred_preview = {
+                    "@type": CRED_PREVIEW_TYPE,
+                    "attributes": [
+                        {"name": n, "value": v}
+                        for (n, v) in agent.cred_attrs[credential_definition_id].items()
+                    ],
+                }
+                offer_request = {
+                    "connection_id": agent.connection_id,
+                    "cred_def_id": credential_definition_id,
+                    "comment": f"Offer on cred def id {credential_definition_id}",
+                    "credential_preview": cred_preview,
+                }
+                await agent.admin_POST("/issue-credential/send-offer", offer_request)
+
+                # TODO issue an additional credential for Student ID
+
+            elif option == "2":
+                log_status("#20 Request proof of degree from alice")
+                req_attrs = [
+                    {"name": "name", "restrictions": [{"issuer_did": agent.did}]},
+                    {"name": "date", "restrictions": [{"issuer_did": agent.did}]},
+                    {"name": "degree", "restrictions": [{"issuer_did": agent.did}]},
+                    {"name": "self_attested_thing"},
+                ]
+                req_preds = [
+                    {
+                        "name": "age",
+                        "p_type": ">=",
+                        "p_value": 18,
+                        "restrictions": [{"issuer_did": agent.did}],
+                    }
+                ]
+                indy_proof_request = {
+                    "name": "Proof of Education",
+                    "version": "1.0",
+                    "nonce": str(uuid4().int),
+                    "requested_attributes": {
+                        f"0_{req_attr['name']}_uuid": req_attr for req_attr in req_attrs
+                    },
+                    "requested_predicates": {
+                        f"0_{req_pred['name']}_GE_uuid": req_pred
+                        for req_pred in req_preds
+                    },
+                }
+                proof_request_web_request = {
+                    "connection_id": agent.connection_id,
+                    "proof_request": indy_proof_request,
+                }
+                await agent.admin_POST(
+                    "/present-proof/send-request", proof_request_web_request
+                )
+
             elif option == "3":
                 msg = await prompt("Enter message: ")
-                if msg:
-                    await agent.admin_POST(
-                        f"/connections/{agent.connection_id}/send-message",
-                        {"content": msg},
-                    )
-            elif option == "4":
-                # handle new invitation
-                log_status("Input new invitation details")
-                await input_invitation(agent)
+                await agent.admin_POST(
+                    f"/connections/{agent.connection_id}/send-message", {"content": msg}
+                )
 
         if show_timing:
             timing = await agent.fetch_timing()
@@ -267,12 +273,12 @@ async def main(start_port: int, show_timing: bool = False):
 if __name__ == "__main__":
     import argparse
 
-    parser = argparse.ArgumentParser(description="Runs an Alice demo agent.")
+    parser = argparse.ArgumentParser(description="Runs a Faber demo agent.")
     parser.add_argument(
         "-p",
         "--port",
         type=int,
-        default=8030,
+        default=8020,
         metavar=("<port>"),
         help="Choose the starting port number to listen on",
     )
